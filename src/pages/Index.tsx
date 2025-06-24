ee0339eeeb79108e41319e6fb0ec41a143e65ba9
unknown <diegoligarrido@gmail.com>
2025-06-23 22:32:41 -0700

    1

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -250,18 +250,14 @@ const Index = () => {
               los usuarios amen
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
-              <Button
-                size="lg"
-                variant="secondary"
-                className="bg-white text-black hover:bg-gray-100"
-              >
+              <Button size="lg" variant="secondary" className=" text-black  ">
                 <Mail className="w-5 h-5 mr-2" />
                 Iniciar un Proyecto
               </Button>
               <Button
                 size="lg"
                 variant="outline"
-                className="border-white text-white hover:bg-white/10"
+                className="bg-transparent border-white text-white hover:bg-white/10"
               >
                 Ver Portafolio
                 <ArrowRight className="w-5 h-5 ml-2" />
